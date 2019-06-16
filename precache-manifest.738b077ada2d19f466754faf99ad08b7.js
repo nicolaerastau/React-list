701ff12a4fa66c165a0c575228c4dc7cc3f9dd46
nicolaerastau <nickqsr@yahoo.com>
2019-06-16 22:47:26 +0200

    Updates

---
+++ precache-manifest.738b077ada2d19f466754faf99ad08b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ae4e954e7f6fbddf03f",
+    "url": "/static/js/main.4a1a0b0e.chunk.js"
+  },
+  {
+    "revision": "2822e1e6695fbf0ae41b",
+    "url": "/static/js/2.44b20be8.chunk.js"
+  },
+  {
+    "revision": "6ae4e954e7f6fbddf03f",
+    "url": "/static/css/main.96e5e9d9.chunk.css"
+  },
+  {
+    "revision": "26e0011d199ab338f726bcf02b058285",
+    "url": "/index.html"
+  }
+];
